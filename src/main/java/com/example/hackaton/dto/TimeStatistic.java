74f5c4e501d3fcd65bddc17383764f8d23db1629
Yaroslav Ilin <FMyb@github.com>
2023-04-16 01:50:51 +0300

    fix time statistic

--- src/main/java/com/example/hackaton/dto/TimeStatistic.java
+++ src/main/java/com/example/hackaton/dto/TimeStatistic.java
@@ -3,16 +3,18 @@ package com.example.hackaton.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @Getter
 public class TimeStatistic {
 	private final String methodName;
 
-	private final Integer methodId;
+	private final UUID methodId;
 
 	private final Double avgBefore;
 
-	private final Double avgCurrent;
+	private final Double avgAfter;
 
 	private final Double percent;
 
